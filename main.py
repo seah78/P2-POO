337757dda9e3e20388114979aa03ea5f4434a6a6
seah78 <github@seah.pro>
2021-05-02 08:23:54 +0200

    main ok

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
 from home import Home
 
-home = Home("http://books.toscrape.com/")
-home.scrape_home()
-home.print_home()
+def main():
+
+	home = Home("http://books.toscrape.com/")
+	home.scrape_home()
+	home.print_home()
+
+if __name__ == "__main__":
+    main()
